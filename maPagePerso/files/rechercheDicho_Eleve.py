d60e1e7f06b98e8801c42ba7fe1d6c45a5d144b1
bouillotvincent <43422054+bouillotvincent@users.noreply.github.com>
2020-02-19 21:26:41 +0100

    Add files via upload

---
+++ maPagePerso/files/rechercheDicho_Eleve.py
@@ -0,0 +1,21 @@
+import random
+import time
+from matplotlib.pyplot import *
+
+maxVal = 2000
+nVal = 1000
+listeNombres = list(range(0,24,2))#sorted(random.choices(range(maxVal),k=nVal))
+
+def rechercheNaive(L: list, elt: int)-> int:
+    """A compléter"""
+    return -1
+
+
+def rechercheDicho(L: list, elt: int)-> int:
+    """A compléter"""
+    return -1
+
+listeNombres = sorted(random.choices(range(maxVal),k=nVal))
+#print(rechercheDicho(listeNombres, 11))
+
+#print(rechercheNaive(listeNombres,12))
