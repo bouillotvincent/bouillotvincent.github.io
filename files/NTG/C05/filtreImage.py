cbe909403fe53765d03029dca638a3edd5ce8d13
bouillotvincent <43422054+bouillotvincent@users.noreply.github.com>
2020-11-04 23:55:01 +0100

    update terminale

---
+++ files/NTG/C05/filtreImage.py
@@ -0,0 +1,29 @@
+dessin_depart= [
+"00011000",
+"01111110",
+"01000001",
+"10000010",
+"10001111",
+"01100010",
+"00011100",
+"00001000",
+]
+
+def affiche(dessin):
+    """ 
+    fait un joli affichage d'un dessin dans le terminal
+    """
+    pass
+
+def rotationAux(px, x, y, t):
+    """ 
+    description de la fonction à ajouter
+    """
+    pass
+
+    
+def rotation(px):
+    """ 
+    description de la fonction à ajouter
+    """
+    pass
