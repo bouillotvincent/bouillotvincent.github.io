b2e3f5093ff5ff74820281d163802e72f4ffc95d
bouillotvincent <43422054+bouillotvincent@users.noreply.github.com>
2020-11-05 09:05:03 +0100

    up

---
+++ files/N1G/C03/chapitre3.py
@@ -0,0 +1,6 @@
+mesNombres = [ 1, 4, 9, 3, 1, 2 ] 
+mesCourses = [ "stylos rouges" , "piles" , "souris pour la salle info" , "claviers" ]
+mesCoordonnées = [ (1,1) , (0,1) , (1,6) ]
+monBordel = ["un texte", 18, 5.4, True, (1,0)]
+
+print(monBordel)
