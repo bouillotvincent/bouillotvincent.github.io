a7f15289f1c1c0de5ebd110b1049988e903aca1e
bouillotvincent <43422054+bouillotvincent@users.noreply.github.com>
2021-02-04 22:30:59 +0100

    up

---
+++ files/N1G/C07/theme2/theme2.py
@@ -0,0 +1,45 @@
+import random
+import timeit
+
+listeNombres = [3, 7, 12, 31, 44, 55, 67, 78, 81]
+
+def rechercheNaive(L: list, elt: int)-> int:
+    ''' Spécifications :
+    Recherche d'un entier elt dans un tableau d'entiers. 
+    Renvoie le nombre de fois où la comparaison a été faite si on trouve l'élément 
+    Renvoie -1 sinon.
+    '''
+    #
+    #
+    #
+    #
+    #
+    return
+
+def rechercheDicho(L: list, elt: int)-> int:
+    ''' Spécifications :
+    ...
+    ...
+    ...
+    '''
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+
+    return 
+
+
+print(rechercheDicho(5, [1,2,5,9,10,14,17,24,41]))
+
+
+tailleTableau = [10**k for k in range(1,6)]
+
+# from matplotlib.pyplot import *
+# x = [1,2,3,4]
+# y = [10, 23, 45, 7]
+# plot(x, y, 'x-r')
+# show()
