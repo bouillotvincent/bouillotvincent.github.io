677a34c7248f7a75d72364871cb82aee79ceaaac
bouillotvincent <43422054+bouillotvincent@users.noreply.github.com>
2021-02-23 19:48:18 +0100

    up Première

---
+++ files/N1G/C07/theme3/theme3.py
@@ -0,0 +1,45 @@
+import timeit
+
+def triSelection(L: list)-> list:
+    """
+    Réalise un tri par sélection
+    Paramètres d'entrée : 
+        - L : tableaux d'entiers
+    """
+    #
+    #
+    #
+    #
+    #
+    return 
+
+listeNombresTriee = triSelection([10, 3, 7 ,5 ,6, 1]) # trie la liste
+print(listeNombresTriee) # affiche la liste
+
+def triInsertion(L: list)-> list:
+    """
+    Réalise un tri par insertion
+    Paramètres d'entrée : 
+        - L : tableaux d'entiers
+    """
+    #
+    #
+    #
+    #
+    #
+    return # A compléter
+
+listeNombresTriee = triInsertion([10, 3, 7 ,5 ,6, 1]) # trie la liste
+print(listeNombresTriee) # affiche la liste
+
+tailleTableau = [ 10**k for k in range(1,6) ]
+tempsSelection = []
+tempsInsertion = []
+tempsSorted = []
+
+# Création d'un graphique
+# from matplotlib.pyplot import *
+# x = [1,2,3,4]
+# y = [10, 23, 45, 7]
+# plot(x, y, 'x-r')
+# show()
