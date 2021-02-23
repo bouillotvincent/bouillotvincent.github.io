3c1e069f387b82e2bf4c20f3963e46d7890add49
bouillotvincent <43422054+bouillotvincent@users.noreply.github.com>
2021-02-23 08:57:46 +0100

    up

--- files/N1G/C07/theme2/theme2.py
+++ files/N1G/C07/theme2/theme2.py
@@ -3,11 +3,15 @@ import timeit
 
 listeNombres = [3, 7, 12, 31, 44, 55, 67, 78, 81]
 
-def rechercheNaive(L: list, elt: int)-> int:
+def rechercheNaive(L: list, but: int)-> int:
     ''' Spécifications :
-    Recherche d'un entier elt dans un tableau d'entiers. 
+    Recherche un entier but dans un tableau d'entiers. 
     Renvoie le nombre de fois où la comparaison a été faite si on trouve l'élément 
-    Renvoie -1 sinon.
+    Renvoie -1 sinon
+
+    Paramètres d'entrée : 
+        - L : tableau d'entiers
+        - but : entier recherché
     '''
     #
     #
@@ -15,6 +19,9 @@ def rechercheNaive(L: list, elt: int)-> int:
     #
     #
     return
+
+print(rechercheNaive([1,2,5,9,10,14,17,24,41], 5))
+
 
 def rechercheDicho(L: list, elt: int)-> int:
     ''' Spécifications :
@@ -33,7 +40,7 @@ def rechercheDicho(L: list, elt: int)-> int:
     return 
 
 
-print(rechercheDicho(5, [1,2,5,9,10,14,17,24,41]))
+print(rechercheDicho([1,2,5,9,10,14,17,24,41], 5))
 
 
 tailleTableau = [10**k for k in range(1,6)]
