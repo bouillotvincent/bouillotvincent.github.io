93f18ec0e682174b3c260176450576660517e36f
bouillotvincent <vincent.bouillot.sa@gmail.com>
2021-06-04 14:34:37 +0200

    Update knacksackPratique_Eleve.py

--- files/N1G/C12/knacksackPratique_Eleve.py
+++ files/N1G/C12/knacksackPratique_Eleve.py
@@ -2,11 +2,13 @@
 dicoObjet = {}
 
 
-# Que font ces deux lignes ?
-tableau_trié = sorted(dicoObjet.items(), key = lambda a: a[1][2], reverse=True)
-dicoObjet = {clé:valeur for clé, valeur in tableau_trié}
+def f(dicoObjet):
+    # Que font ces deux lignes ?
+    tableau_trié = sorted(dicoObjet.items(), key = lambda a: a[1][2], reverse=True)
+    return {clé:valeur for clé, valeur in tableau_trié}
 
 print(dicoObjet)
+print(f(dicoObjet))
 
 aVoler = []
 poidsMax = 30
