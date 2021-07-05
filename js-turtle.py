e1c4aa205edfd38bd0b64e302f6bfdc827b72bd9
bouillotvincent <43422054+bouillotvincent@users.noreply.github.com>
2021-07-05 09:57:05 +0200

    Update js-turtle.py

--- js-turtle.py
+++ js-turtle.py
@@ -1,4 +1,28 @@
 from js import document
+import inspect
+
+class Tortue:
+
+	def __init__(self, context, x = 0, y = 0):
+		self.x, self.y = x, y
+		self.color = '#ff0000'
+		self.width = 1
+		self.style = '🐢'
+
+	def pencolor(self, *args):
+		if len(args) not in [0,1] : 
+			raise TypeError(f"""{inspect.currentframe().f_code.co_name} takes 1 positional argument but {len(args)} were given""")
+		if len(args) == 0:
+			return self.color
+		else:
+			self.color = args
+			
+	def forward(self):
+		context.fillStyle = self.color
+		context.beginPath()
+		context.moveTo(10, 10)
+		context.lineTo(310, 310)
+		context.stroke()
 
 canvas = document.querySelector('canvas')
 canvas.setAttribute('width', 640)
@@ -11,25 +35,27 @@ pen = False
 lastPoint = (0, 0)
 
 def onmousemove(e):
-  global lastPoint
-  if pen:
-    newPoint = (e.offsetX, e.offsetY)
-    context.beginPath()
-    context.moveTo(lastPoint[0], lastPoint[1])
-    context.lineTo(newPoint[0], newPoint[1])
-    context.closePath()
-    context.stroke()
-    lastPoint = newPoint
-    
+	global lastPoint
+	if pen:
+		newPoint = (e.offsetX, e.offsetY)
+		context.beginPath()
+		context.moveTo(lastPoint[0], lastPoint[1])
+		context.lineTo(newPoint[0], newPoint[1])
+		context.closePath()
+		context.stroke()
+		lastPoint = newPoint
+	
 def onmousedown(e):
-  global pen, lastPoint
-  pen = True
-  lastPoint = (e.offsetX, e.offsetY)
+	global pen, lastPoint
+	pen = True
+	lastPoint = (e.offsetX, e.offsetY)
   
 def onmouseup(e):
-  global pen
-  pen = False
+	global pen
+	pen = False
 
 canvas.addEventListener('mousemove', onmousemove)
 canvas.addEventListener('mousedown', onmousedown)
 canvas.addEventListener('mouseup', onmouseup)
+
+Tortue(context)
